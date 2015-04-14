4b4e8603c8703318322af45be92a302d57cee084
brownrout <ericbrownrout2016@u.northwestern.edu>
2015-04-14 11:13:39 -0500

    Made a Mistake
    Some of our work from yesterday was undone. So I just redid the parts
    that were off.

--- app/message/scripts/IndexController.js
+++ app/message/scripts/IndexController.js
@@ -3,19 +3,12 @@ angular
   .controller("IndexController", function ($scope, Message, supersonic) {
     $scope.messages = null;
     $scope.showSpinner = true;
-    $scope.myMessages = [];
     $scope.phoneNumber = localStorage.getItem("user-phone");
 
     Message.all().whenChanged( function (messages) {
         $scope.$apply( function () {
           $scope.messages = messages;
           $scope.showSpinner = false;
-
-          for(var i=0;i<messages.length;i++){
-            if($scope.messages[i]['phone']===$scope.phoneNumber)
-               $scope.myMessages.push($scope.messages[i]);
-          }
-
         });
     });
   });

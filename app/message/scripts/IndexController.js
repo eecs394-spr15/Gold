7f5a3eb8a367c96d758df85f8dd4d35104f9f123
Jiwei Xia <jiweixia2016@u.northwestern.edu>
2015-04-18 21:43:58 -0500

    Date Picker and Search by Date range.
    Date Picker section added and can search by date range.

--- app/message/scripts/IndexController.js
+++ app/message/scripts/IndexController.js
@@ -2,21 +2,31 @@ angular
   .module('message')
   .controller("IndexController", function ($scope, Message, supersonic) {
     $scope.messages = null;
-    $scope.showSpinner = true;
+    $scope.showSpinner = false;
     $scope.myMessages = [];
     $scope.phoneNumber = localStorage.getItem("user-phone");
     $scope.keyword = "";
-    $scope.fullHeartURL = "http://i60.tinypic.com/10crg5k.png";
-    $scope.halfHeartURL = "http://i57.tinypic.com/23vyzk5.png";
-    $scope.emptyHeartURL ="http://i58.tinypic.com/2akg4mv.png";
+
     $scope.searchByMoodRatingLowerBound =0;
     $scope.searchByMoodRatingUpperBound =5;
 
+    $scope.searchByDateLowerBound = new Date('1995-12-17T03:24:00');
+    $scope.searchByDateUpperBound = new Date();
+
+    $scope.bool = false;
+    $scope.boollist =[];
     $scope.filterFunction = function(element) {
+        var messageDate = new Date(element.date);
+        messageDate.getUTCFullYear(); //1995-12-17
+                                      //2005-4-20
+                                      //2005-5-20
+                                      $scope.boollist.push(messageDate.getTime()>=$scope.searchByDateLowerBound.getTime() && messageDate.getTime()<=$scope.searchByDateUpperBound.getTime());
       return  ((element.date.match($scope.keyword) ? true : false)
       ||      (element.text.toLowerCase().match($scope.keyword.toLowerCase()) ? true : false))
-      && (element.rating>=$scope.searchByMoodRatingLowerBound) && (element.rating<=$scope.searchByMoodRatingUpperBound ) ;
-    };
+      && (element.rating>=$scope.searchByMoodRatingLowerBound) && (element.rating<=$scope.searchByMoodRatingUpperBound )
+      && messageDate.getTime()>=$scope.searchByDateLowerBound.getTime() && messageDate.getTime()<=$scope.searchByDateUpperBound.getTime();
+      //&& (messageDate.getTime()>=$scope.searchByDateLowerBound.getTime()) && (messageDate.getTime()<=$scope.searchByDatUpperBound.getTime());
+    }
 
     $scope.changeLowerBound =function(){
       if($scope.searchByMoodRatingLowerBound>$scope.searchByMoodRatingUpperBound ){
@@ -28,6 +38,14 @@ angular
          $scope.searchByMoodRatingLowerBound=$scope.searchByMoodRatingUpperBound ;
       }   
     }
+    $scope.underline = function(value){
+      if(value == true) return {"text-decoration":"underline"};
+      else return {"text-decoration":"none"};
+    }
+
+
+
+
 
     Message.all().whenChanged( function (messages) {
         $scope.$apply( function () {
@@ -40,5 +58,12 @@ angular
           }
         });
     });
+
+
+
+
+    $scope.fullHeartURL = "http://i60.tinypic.com/10crg5k.png";
+    $scope.halfHeartURL = "http://i57.tinypic.com/23vyzk5.png";
+    $scope.emptyHeartURL ="http://i58.tinypic.com/2akg4mv.png";
   });
 
